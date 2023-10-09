1c60193083dbf93ac714c0c04c69bfc703ba0d82
Liyulin <1471127927@qq.com>
2023-10-09 18:09:04 +0800

    test git ignore

--- project/Assets/Code/RobotControler/RobotState/CameraState.cs
+++ project/Assets/Code/RobotControler/RobotState/CameraState.cs
@@ -2,7 +2,7 @@ using Code.RobotControler.Senser;
 
 namespace Code.RobotControler.RobotState
 {
-    public abstract class CameraState
+    public abstract class CameraState 
     {
         public FakeCamera camera;
 
---
+++ project/Assets/Code/RobotControler/RobotState/Car_auto_rotation_state.cs
@@ -0,0 +1,7 @@
+namespace Code.RobotControler.RobotState
+{
+    public class Car_auto_rotation_state
+    {
+        
+    }
+}
